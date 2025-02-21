c05fadbf37b1fedbd121852f85bd78e162cb3afe
Timeless Residents <business@satotakuya.jp>
2025-02-21 19:09:55 +0900

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box p={5}>
+      <Heading>Chakra UIのテスト</Heading>
+      <Text mt={3}>これはChakra UIを使った簡単なテストです</Text>
+    </Box>
+  );
+}
+
+export default App;
